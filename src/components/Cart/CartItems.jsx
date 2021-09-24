383092659a272f888d99efb83b68c3f8d0aad27a
jspw <mhshifat757@gmail.com>
2021-09-25 02:04:30 +0600

    changes added in cart modal content

--- src/components/Cart/CartItems.jsx
+++ src/components/Cart/CartItems.jsx
@@ -1,16 +1,20 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import CartItem from "./CartItem";
+import shoppingIcon from "../../assets/images/shopping.png";
 
 export default function CartItems() {
   const { cart } = useContext(ShopContext);
-  return (
-    cart && (
-      <div className="flex flex-col space-y-2">
-        {cart.products.map((product) => (
-          <CartItem key={product.id} product={product} />
-        ))}
-      </div>
-    )
+  return cart ? (
+    <div className="flex flex-col space-y-2">
+      {cart.products.map((product) => (
+        <CartItem key={product.id} product={product} />
+      ))}
+    </div>
+  ) : (
+    <div className="container flex flex-col space-y-2 items-center justify-center  ">
+      <img src={shoppingIcon} className="rounded-full h-48 w-48" />
+      <p className="text-gray-500">Your cart is empty. Start shopping</p>
+    </div>
   );
 }
--- src/components/Cart/CartModal.jsx
+++ src/components/Cart/CartModal.jsx
@@ -1,9 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import CartItems from "./CartItems";
+import CancelIcon from "@mui/icons-material/Cancel";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
 
 export default function CartModal({ showCartModal, handleCartModal }) {
   const { cart } = useContext(ShopContext);
+  const [isPromoValid, setPromoValid] = useState(false);
   return (
     <div
       id="modal"
@@ -13,15 +16,22 @@ export default function CartModal({ showCartModal, handleCartModal }) {
     >
       <div className="fixed right-0 bg-white  h-full overflow-scroll duration-500 animate-slideIn flex flex-col justify-between  sm:w-full md:w-2/5 lg:w-4/12 xl:w-1/4 ">
         <div>
-          <div className="sticky top-0 bg-white z-40">
-            <div className="flex flex-row justify-between items-baseline ">
-              <h1 className="p-4">My Cart</h1>
-              <button
-                onClick={handleCartModal}
-                className="btn p-4 text-xl text-gray-400"
-              >
-                x
-              </button>
+          <div className="sticky top-0 bg-gray-400 z-40 ">
+            <div className="flex flex-row justify-between items-center ">
+              <div className="p-4 flex flex-row items-center space-x-2">
+                <LocalMallIcon />
+                <div className="text-lg font-semibold">
+                  {cart ? cart.products.length : 0} items
+                </div>
+              </div>
+              <div>
+                <button
+                  onClick={handleCartModal}
+                  className="btn btn-close p-4 text-xl text-gray-400"
+                >
+                  <CancelIcon htmlColor="white" />
+                </button>
+              </div>
             </div>
             <hr />
           </div>
@@ -30,9 +40,31 @@ export default function CartModal({ showCartModal, handleCartModal }) {
 
         {cart && (
           <div className="flex flex-col sticky bottom-0">
-            <button className="flex flex-row justify-between btn bg-blue-400 rounded font-semibold text-white ml-2 mr-2 mt-2 p-2">
+            <div className="bg-gray-200 flex flex-row">
+              <div>
+                <input
+                  placeholder="Promo Code"
+                  className="outline-none appearance-none mb-3 ml-1 mr-1 mt-2 rounded p-2 max-w-min"
+                />
+              </div>
+              <div>
+                <button
+                  onClick={() => setPromoValid(true)}
+                  className="btn bg-blue-700 text-white rounded m3-2 ml-1 mr-1 mt-2 p-2"
+                >
+                  Go
+                </button>
+              </div>
+            </div>
+
+            <button className="flex flex-row justify-between btn bg-blue-400 font-semibold text-white p-2">
               <p> Order Now</p>
-              <p className="font-bold">${Math.round(cart.payableAmount)}</p>
+              <p className="font-bold">
+                $
+                {isPromoValid
+                  ? Math.round(cart.payableAmount) - 100
+                  : Math.round(cart.payableAmount)}
+              </p>
             </button>
           </div>
         )}
