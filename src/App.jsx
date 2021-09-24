bcba61c508a4ee0bafd6d25121ed1b618f977474
jspw <mhshifat757@gmail.com>
2021-09-25 02:30:33 +0600

    404 page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
+import Page404 from "./components/Page404/Page404";
 import CategoryContextProvider from "./Context/CategoryContext";
 import ShopContextProvider from "./Context/ShopContext";
 
@@ -12,8 +13,14 @@ function App() {
         <BrowserRouter>
           <NavBar />
           <Switch>
-            <Route path="/">
+            <Route path="/" exact>
               <Home />
+            </Route>
+            <Route path="/category/:category" exact></Route>
+            <Route path="/product/:product" exact></Route>
+            <Route path="/order" exact></Route>
+            <Route path="*">
+              <Page404 />
             </Route>
           </Switch>
           <Footer />
