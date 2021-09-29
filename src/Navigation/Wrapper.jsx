47375976bf375dfcaf0b9add9c6d18655b2bc847
jspw <mhshifat757@gmail.com>
2021-09-29 15:42:34 +0600

    scrolling issue fixed

--- src/Navigation/Wrapper.jsx
+++ src/Navigation/Wrapper.jsx
@@ -3,12 +3,14 @@ import AppRoutes from "./AppRoutes";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 import useFetch from "../hooks/useFetch";
+import TopScroller from "../components/TopScroller/TopScroller";
 
 export default function Wrapper() {
   useFetch();
 
   return (
     <BrowserRouter>
+      <TopScroller />
       <div className="flex flex-col justify-between h-screen">
         <div>
           <NavBar />
